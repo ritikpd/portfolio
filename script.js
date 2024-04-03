671c737bca0f943d8003f70de7045af6152b8757
ritik <rdp382847@gmail.com>
2024-04-03 22:10:26 +0530

    commit changes

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
-var tl = gsap.timeline()
-tl.from('#nav h1',{
-    y: -50,
-    opacity: 0,
-    // delay: 0.4,
-    duration: 0.0, 
-    stagger: 0.3
-})
-tl.from('#nav ',{
-    y: -50,
-    opacity: 0,
-    delay: 0.4,
-    duration: 0.2, 
-    stagger: 0.3
-})
-tl.from('#main h2', {
-    x: -500,
-    delay: 0.5,
-    opacity:0,
-    duration: 0.2, 
-    stagger: 0.4
-})
+const section = document.querySelectorAll("section");
+const navlinks = document.querySelectorAll("header nav a");
 
+window.onscroll = () => {
+  section.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navlinks.forEach((links) => {
+        links.classList.remove("active");
+        document.querySelector("header nav a[href*='+id+']").classList.add("active");
+      });
+    }
+  });
+};
