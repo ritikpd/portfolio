38aaf4002a12b2067ca832c0b6756a0fb06a447f
ritik <rdp382847@gmail.com>
2024-04-04 07:45:29 +0530

    pushing given file into github index.html, style.css, script.js,ritik, ritik.txt

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const section = document.querySelectorAll("section");
 const navlinks = document.querySelectorAll("header nav a");
 
@@ -17,23 +17,3 @@ window.onscroll = () => {
     }
   });
 };
-=======
-const section = document.querySelectorAll("section");
-const navlinks = document.querySelectorAll("header nav a");
-
-window.onscroll = () => {
-  section.forEach((sec) => {
-    let top = window.scrollY;
-    let offset = sec.offsetTop - 150;
-    let height = sec.offsetHeight;
-    let id = sec.getAttribute("id");
-
-    if (top >= offset && top < offset + height) {
-      navlinks.forEach((links) => {
-        links.classList.remove("active");
-        document.querySelector("header nav a[href*='+id+']").classList.add("active");
-      });
-    }
-  });
-};
->>>>>>> cf0869a02baee5d96a22d1127e9ab9355003bbc4
